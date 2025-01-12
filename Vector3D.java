7b71ed0e1a77cb697ba467770e69ad01c83235cc
Katherine Jordan <happy.green.bunny@gmail.com>
2025-01-11 21:25:50 -0800

    Step 1 and step 2

---
+++ Vector3D.java
@@ -0,0 +1,26 @@
+import java.util.Vector;
+
+public class Vector3D {
+    double x;
+    double y;
+    double z;
+
+    public Vector3D(double x, double y, double z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public double getZ() {
+        return z;
+    }
+
+}
